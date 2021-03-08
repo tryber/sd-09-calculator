4cf216eee1f44ac0a88cc20821f083ad9cbe37f1
nato-re <alvares.renato@hotmail.com>
2021-03-08 18:14:22 -0300

    now i saved

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 function sum(a, b) {
-<<<<<<< HEAD
-    return a - b;
+    return a + b;
 }
 
 function division(a, b){
     return a / b;
-=======
-    return a + b;
 }
 
 function subtraction(a, b) {
     return a - b;
->>>>>>> aa990f111a45ddda634afbf51dd6484cc83a25e8
 }
